a0b664a31c71cf49f3db698efb2fbac0d7535d0e
yurov-37 <104888831+yurov-37@users.noreply.github.com>
2022-09-28 10:32:40 +0000

    Deploying to gh-pages from @ yurov-37/goit-js-hw-09@f7986cfa5d18267ad9bbed2c25c459ffc21c2d01 🚀

---
+++ 01-color-switcher.3b636056.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");e.setAttribute("disabled","true");let d=null;t.addEventListener("click",(function(){t.setAttribute("disabled","true"),e.removeAttribute("disabled"),d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.removeAttribute("disabled"),e.setAttribute("disabled","true")}));
+//# sourceMappingURL=01-color-switcher.3b636056.js.map
